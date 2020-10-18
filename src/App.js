6e3eda0e81d2e493579a2b482c6fee3b76ac59a4
Marco Austria <maustria0507@gmail.com>
2020-10-17 22:05:04 -0400

    added some css, navbar, background image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+  Link
+} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Homepage from "./components/Homepage"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hey Palmeasy
-        </a>
-      </header>
+        <Homepage/>
+       <Router>
+          <Switch>
+              <Route exact path='/' component={Homepage} />
+              {/* <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} /> */}
+          </Switch>
+       </Router>
     </div>
   );
 }
